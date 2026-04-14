e82bdcd7923752bef75b054d060276ed886b470a
lobotomiaah <maxwelferreira@proton.me>
2026-04-13 22:07:03 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    const checkAcompanhante = document.getElementById('check-acompanhante');
+    const areaQuantidade = document.getElementById('area-quantidade');
+    const inputQTD = document.getElementById('qtd-pessoas');
+    const btnConfirmar = document.getElementById('btn-confirmar');
+    checkAcompanhante.addEventListener('change', function() {
+        if (this.checked) {
+            areaQuantidade.style.display = 'block';
+        } else {
+            areaQuantidade.style.display = 'none';
+        }
+    });
+    btnConfirmar.addEventListener('click', function() {
+        let total = 1
+        if(checkAcompanhante.checked){
+            total = 1 + Number(inputQTD.value)
+        }   
+        console.log('o total de pessoas é: '+total)
+        alert("presença confirmada para: "+total+"pessoas(s)!   ")
+    });
